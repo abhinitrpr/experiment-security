356fcd6f58c00ebeecdcef437a7dc46c52db37b9
abhishekkumar <lovestory1990@gmail.com>
2021-11-23 07:21:27 +0530

    error page

--- application/models.py
+++ application/models.py
@@ -2,7 +2,7 @@ from enum import unique
 from .database import db
 from flask_security import UserMixin, RoleMixin
 
-role_users = db.Table(
+roles_users = db.Table(
     "roles_users",
     db.Column("user_id", db.Integer(), db.ForeignKey("user.id")),
     db.Column("role_id", db.Integer(), db.ForeignKey("role.id")),
@@ -14,8 +14,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     username = db.Column(db.String)
     email = db.Column(db.String, unique=True)
-    password = db.column(db.String(255))
-    active = db.Coulmn(db.Boolean())
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
     roles = db.relationship(
         "Role", secondary=roles_users, backref=db.backref("users", lazy="dynamic")
     )
--- main.py
+++ main.py
@@ -6,15 +6,20 @@ from flask_restful import Resource, Api
 from application import config
 from application.config import LocalDevelopmentConfig
 from application.database import db
-from flask_security import Security, SQLAlchemySessionUserDatastore, SQLAlchemyUserDatastore
+from flask_security import (
+    Security,
+    SQLAlchemySessionUserDatastore,
+    SQLAlchemyUserDatastore,
+)
 from application.models import User, Role
+
 app = None
 api = None
 
 
 def create_app():
     app = Flask(__name__, template_folder="templates")
-    if os.getenv('ENV', "development") == "production":
+    if os.getenv("ENV", "development") == "production":
         raise Exception("Currently no production config is setup.")
     else:
         print("Staring Local Development")
@@ -31,9 +36,17 @@ app, api = create_app()
 
 # Import all the controllers so they are loaded
 from application.controllers import *
+
 # Add all restful controllers
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
 api.add_resource(UserAPI, "/api/user", "/api/user/<string:username>")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Run the Flask app
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
